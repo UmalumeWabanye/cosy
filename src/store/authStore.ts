3322c7703e03c9c49792b16b14d2cada51f3c506
Apple <apple@Malumes-MBP.lan>
2026-03-31 14:07:55 +0200

    Add logout method to authStore

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -14,6 +14,7 @@ interface AuthStore {
   isAuthenticated: boolean;
   setUser: (user: User | null) => void;
   setToken: (token: string | null) => void;
+  logout: () => void;
 }
 
 export const useAuthStore = create<AuthStore>((set) => ({
@@ -22,4 +23,5 @@ export const useAuthStore = create<AuthStore>((set) => ({
   isAuthenticated: false,
   setUser: (user) => set({ user }),
   setToken: (token) => set({ token, isAuthenticated: !!token }),
+  logout: () => set({ user: null, token: null, isAuthenticated: false }),
 }));
