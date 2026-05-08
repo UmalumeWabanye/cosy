8b84d076fc6136674e7f90e5f035edf2608e7c0e
Apple <apple@Malumes-MBP.lan>
2026-05-08 15:25:13 +0200

    fix(next): merge duplicate nextConfig declarations

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
 };
 
 module.exports = nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
